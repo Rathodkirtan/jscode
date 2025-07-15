044df16f7bff0afa7000ec089227bff0ea0b01a7
Rathodkirtan <Kirtan12082004@gmail.com>
2025-07-15 07:20:33 +0000

    conersion

---
+++ basic/conversen.js
@@ -0,0 +1,11 @@
+// let a=true;
+// a=null;
+// a=undefined;
+//null=>0 undefined=>NaN true=1 false=0 "3ddd"=>NaN
+
+// a="23nn"
+// let aNum=Number(a);
+// console.log(aNum)
+
+// ""=false,"kirtan"=>true,[]=>true,1 0=>true,false
+console.log(Boolean([]))
